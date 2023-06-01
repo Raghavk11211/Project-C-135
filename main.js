1357325db6cb4f1f252bd1ac842e3206d8ead140
Raghavk11211 <90191254+Raghavk11211@users.noreply.github.com>
2023-06-01 20:58:11 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 img = "";
 status1 = "";
 resultarr = [];
+input = "";
 
 function setup() {
     canvas = createCanvas(380, 300);
@@ -8,6 +9,19 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(380,300);
     video.hide();
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input = document.getElementById("Object Text");
+}
+function preload() {
+    img = loadImage('download.jpg');
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status1 = true;
+    objectDetector.detect(video, gotResult);
 }
 function draw() {
     image(video, 0, 0, 380, 300);
